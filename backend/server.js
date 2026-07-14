072f5d8deae5d186fc68a63bd443c16b84a46cba
alishasedai <alishasedai21@gmail.com>
2026-07-15 00:17:48 +0545

    backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+//app config
+
+const app = express();
+const port = 4000;
+
+//midleware 
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.get("/",(req,res) => {
+    res.send("API Working");
+})
+app.listen(port,() => {
+    console.log(`Server started on http://localhost:${port}`)
+})
+//
