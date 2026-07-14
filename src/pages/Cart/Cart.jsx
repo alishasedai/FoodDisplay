8bce6f56f8cfaf3eed32e157de4c7bf0f0ef7e24
alishasedai <alishasedai21@gmail.com>
2026-07-14 15:24:07 +0545

    total price

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -5,7 +5,7 @@ const Cart = () => {
   const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
 console.log(cartItems)
   return (
-    <div className='cart mt-2 '>
+    <div className='cart mt-[100px] mb-[100px] '>
       <div className='cart-items '>
         <div className="cart-items-title grid grid-cols-6 text-gray-600 h-6 items-center ">
           <p>Items</p>
@@ -22,12 +22,12 @@ console.log(cartItems)
             return (
               <div>
                 <div className=' h-12 grid grid-cols-6 items-center mt-2 mb-2 text-black'>
-                <img src={item.image} className='rounded-sm w-[60px] h-[50px]' alt="" />
+                <img src={item.image} className='rounded-sm w-[40px] h-[40px] font-black' alt="" />
                 <p>{item.name}</p>
                 <p>${item.price}</p>
                 <p> {cartItems[item._id]}</p>
                 <p> ${item.price * cartItems[item._id]}</p>
-                <p>x</p>
+                <p onClick={() => removeFromCart(item._id)} className='cursor-pointer'>x</p>
                 
               </div>
               <hr />
@@ -35,8 +35,41 @@ console.log(cartItems)
             )
           }
         })}
-      </div>
+        
+         
+          </div>
+          <div className="cart-bottom mt-[60px] flex justify-between gap-20 ">
+            <div className="total flex flex-col w-1/2">
+              <h2 className='font-bold text-2xl mb-2'>Cart Totals</h2>
+              <div>
+                <div className="cart-total-details flex justify-between text-black font-extrabold">
+                  <p>Sub Totals</p>
+                  <p>{0}</p>
+                </div>
+                <hr className='mt-[8px] mb-2 text-gray-400'/>
+                <div className='cart-totals-sub flex justify-between text-black font-extrabold'>
+                  <p>Delivery Fee</p>
+                  <p>{2}</p>
+                </div>
+                <hr className='mt-[8px]  text-gray-400' />
+                <div className='cart-totals flex justify-between text-black font-extrabold'>
+                <b>Total</b>
+                <b>{0}</b>
+                </div>
+              </div>
+            <button className='w-[200px] py-[10px] mt-3 rounded-sm bg-[#ff6347] text-white cursor-pointer'>PROCEED TO CHECKOUT</button>
 
+            </div>
+            <div className="promoCode flex-1">
+              <div>
+                <p className='mb-2'>If you have a promo code, Enter it here.</p>
+                <div className="inputs w-full ">
+                  <input type="text" className='md:w-[360px] w-[120px] mb-3 bg-gray-200 p-2' placeholder='promo code' />
+                  <button className='p-2 w-30 rounded-md text-white bg-gray-800'>Submit</button>
+                </div>
+              </div>
+            </div>
+          </div>
     </div>
   )
 }
