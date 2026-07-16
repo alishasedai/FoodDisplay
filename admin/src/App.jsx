7966b0f90c4882bbac33e6df2e3221d6c2ce691c
alishasedai <alishasedai21@gmail.com>
2026-07-17 01:09:24 +0545

    added the items through the frontend

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import {Route, Routes} from "react-router-dom";
+import Add from './pages/Add';
+import List from './pages/List';
+import Orders from './pages/Orders';
+ import { ToastContainer, toast } from 'react-toastify';
+
 
 const App = () => {
   return (
     <div className='w-[90%] mx-auto'>
+      <ToastContainer />
       <Navbar />
       <hr />
      <div className='app-content flex '>
        <Sidebar />
+       <Routes>
+        <Route path='/add' element={<Add />}/>
+        <Route path='/list' element={<List />}/>
+        <Route path='/orders' element={<Orders />}/>
+
+       </Routes>
      </div>
     </div>
   )
